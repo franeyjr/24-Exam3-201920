39786523d58bbe8a57001cfe8a277289ff3dad27
franeyjr <franeyjr@rose-hulman.edu>
2019-01-31 21:03:37 -0500

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -126,7 +126,7 @@ def problem1(n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # **** IMPORTANT ****:
     # We supplied a   fibonacci   function above.
@@ -152,7 +152,7 @@ def problem1(n):
     answer = 0
     seq = [0,1]
     i = 0
-    while answer < n:
+    while seq[i] < n:
         seq = seq + [seq[i] + seq[i+1]]
         i = i + 1
         answer = seq[i-1]
